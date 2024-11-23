433f327989c3ded85581ebc8a5b309ea3e0be66b
hyunjun-17 <wlfkem159@gmail.com>
2024-11-23 14:59:31 +0900

    24.11.23 15:

--- Python/pjfunction.py
+++ Python/pjfunction.py
@@ -4,7 +4,7 @@ from langchain_pinecone import PineconeVectorStore
 from langchain_openai import OpenAIEmbeddings
 from langchain import hub
 
-def get_llm(model='gpt-4-turbo'):
+def get_llm(model='gpt-4o'):
     llm = ChatOpenAI()
     return llm
 
